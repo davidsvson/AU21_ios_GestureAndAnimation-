5bf6eb92b527f23c32e584ef1de7513e0044c1d9
David Svensson <davidsvson@gmail.com>
2021-11-30 13:58:23 +0100

    first commit

--- GestureAndAnimation/ViewController.swift
+++ GestureAndAnimation/ViewController.swift
@@ -9,11 +9,54 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var singleTapRecognizer: UITapGestureRecognizer!
+    @IBOutlet var doubleTapRecognizer: UITapGestureRecognizer!
+    
+    
+    @IBOutlet weak var gestureNameLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        singleTapRecognizer.require(toFail: doubleTapRecognizer)
+        
     }
+    
+    
+    @IBAction func handleTap(_ sender: UITapGestureRecognizer) {
+       showGesture(name: "tap")
+    
 
-
+        
+    }
+    
+    
+    @IBAction func handleDoubleTap(_ sender: UITapGestureRecognizer) {
+        showGesture(name: "tap tap")
+        
+    }
+    
+    
+    func showGesture(name: String) {
+        gestureNameLabel.text = name
+        
+        UIView.animate(withDuration: 1, animations: { self.gestureNameLabel.alpha = 1.0 }, completion: animamateLabelInvicible(finished:))
+        
+    }
+    
+    func animamateLabelInvicible(finished: Bool) {
+        UIView.animate(withDuration: 1, animations: { self.gestureNameLabel.alpha = 0.0 }, completion: nil)
+    }
+    
+    
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+      
+        if motion == .motionShake {
+            showGesture(name: "shake")
+        }
+    }
+    
 }
+
+
 
